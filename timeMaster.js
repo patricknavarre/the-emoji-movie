a7a7dc4bac1b7df6084531da81e9f2435f180ac0
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-11-12 10:23:37 -0500

    emoji-movie-completed

---
+++ timeMaster.js
@@ -0,0 +1,11 @@
+const day = require('./day.js');
+
+console.log('Have a great day!!');
+
+let userInput = process.argv(3)
+const userInput = process.argv(3) * 1000;
+
+let i = 0
+
+const whatADay = 
+
